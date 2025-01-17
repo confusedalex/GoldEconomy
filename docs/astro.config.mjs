67207ea44cef61e73e2ae48aa3b4e0f4a6a4f84c
confusedalex <alex@confusedalex.dev>
2025-01-17 22:29:40 +0100

    docs: added site using astro and starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+// https://astro.build/config
+export default defineConfig({
+	site: "https://thegoldeconomy.confusedalex.dev",
+	integrations: [
+		starlight({
+			title: 'TheGoldEconomy',
+			description: 'TheGoldEconomy is a simple, easy to use, and powerful economy plugin for Minecraft servers.',
+			editLink: {
+				baseUrl: 'https://github.com/confusedalex/goldeconomy-docs/edit/main/docs',
+			},
+			logo: {
+				src: './src/assets/logo.svg',
+				replacesTitle: true,
+			},
+			social: {
+				github: 'https://github.com/confusedalex/goldeconomy',
+			},
+			sidebar: [
+				{
+					label: 'Start Here',
+					autogenerate: { directory: 'start-here' }
+				}
+			],
+		}),
+	],
+
+});
---
+++ docs/src/content/docs/start-here/index.md
@@ -0,0 +1,12 @@
+---
+title: Installation
+slug: installation
+---
+
+To install TheGoldEconomy you need to follow these steps:
+1. Download the latest release from [Modrinth](https://modrinth.com/plugin/thegoldeconomy)
+2. Download the Vault dependency from [Spigot](https://www.spigotmc.org/resources/vault.34315/)
+3. Put both files in your `plugins` folder
+4. Restart your server to generate the config file
+5. Edit the config file to your liking
+6. Have fun!
