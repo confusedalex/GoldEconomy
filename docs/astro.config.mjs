627a387f37b9e573bc71cc1ea6100d548b5f9745
confusedalex <alex@confusedalex.dev>
2025-01-18 12:25:54 +0100

    style: remove comments

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-// https://astro.build/config
+
 export default defineConfig({
 	site: "https://thegoldeconomy.confusedalex.dev",
 	integrations: [
