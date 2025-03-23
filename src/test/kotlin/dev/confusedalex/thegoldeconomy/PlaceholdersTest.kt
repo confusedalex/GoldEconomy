17cb2aa1e89865c779e7e42b863d59e8863dee9e
confusedalex <alex@confusedalex.dev>
2025-03-23 01:46:14 +0100

    tests: add some tests

--- src/test/kotlin/dev/confusedalex/thegoldeconomy/PlaceholdersTest.kt
+++ src/test/kotlin/dev/confusedalex/thegoldeconomy/PlaceholdersTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInfo
 import org.mockbukkit.mockbukkit.MockBukkit
 import org.mockbukkit.mockbukkit.ServerMock
 
@@ -15,14 +14,12 @@ class PlaceholdersTest {
 
     @BeforeEach
     fun setUp() {
-        // Start the mock server
         server = MockBukkit.mock()
-        // Load your plugin
         plugin = MockBukkit.load(TheGoldEconomy::class.java)
     }
 
     @AfterEach
-    fun tearDown(testInfo: TestInfo) { // Stop the mock server
+    fun tearDown() { // Stop the mock server
         MockBukkit.unmock()
     }
 
@@ -48,5 +45,25 @@ class PlaceholdersTest {
     fun persist() {
         val placeholders = Placeholders(plugin)
         assertTrue(placeholders.persist())
+    }
+
+    @Test
+    fun onRequest() {
+        val placeholders = Placeholders(plugin)
+        val player = server.addPlayer()
+        val uuid = player.uniqueId
+        val converter = plugin.eco.converter
+        val bank = plugin.eco.bank
+
+        bank.setAccountBalance(uuid, 1000)
+        assertEquals("0", placeholders.onRequest(player, "inventorybalance"))
+        assertEquals("1000", placeholders.onRequest(player, "bankbalance"))
+        assertEquals("1000", placeholders.onRequest(player, "totalbalance"))
+        converter.withdraw(player, 500, Base.NUGGETS)
+        assertEquals("500", placeholders.onRequest(player, "inventorybalance"))
+        assertEquals("500", placeholders.onRequest(player, "bankbalance"))
+        assertEquals("1000", placeholders.onRequest(player, "totalbalance"))
+
+        assertEquals(null, placeholders.onRequest(player, "someNoneExistingPlaceholder"))
     }
 }
