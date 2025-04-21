7fe097e8e3a2e9dbdbc8d272aa7cfe5a133032d2
confusedalex <alex@confusedalex.dev>
2025-04-21 19:02:52 +0200

    fix(astro): adapt to breaking change in 0.33

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,26 +2,23 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-	site: "https://thegoldeconomy.confusedalex.dev",
-	integrations: [
-		starlight({
-			title: 'TheGoldEconomy',
-			description: 'TheGoldEconomy is a simple, easy to use, and powerful economy plugin for Minecraft servers.',
-			editLink: {
-				baseUrl: 'https://github.com/confusedalex/goldeconomy/edit/main/docs',
-			},
-			logo: {
-				src: './src/assets/logo.svg',
-				replacesTitle: true,
-			},
-			social: {
-				github: 'https://github.com/confusedalex/goldeconomy',
-			},
-			sidebar: [
-				'installation', 'configuration', 'commands', 'permissions',
-			],
-			lastUpdated: true,
-		}),
-	],
-
+  site: "https://thegoldeconomy.confusedalex.dev",
+  integrations: [
+    starlight({
+      title: 'TheGoldEconomy',
+      description: 'TheGoldEconomy is a simple, easy to use, and powerful economy plugin for Minecraft servers.',
+      editLink: {
+        baseUrl: 'https://github.com/confusedalex/goldeconomy/edit/main/docs',
+      },
+      logo: {
+        src: './src/assets/logo.svg',
+        replacesTitle: true,
+      },
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/confusedalex/goldeconomy' }],
+      sidebar: [
+        'installation', 'configuration', 'commands', 'permissions',
+      ],
+      lastUpdated: true,
+    }),
+  ],
 });
